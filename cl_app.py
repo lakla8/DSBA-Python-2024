412d000c0c86d3b7ff1e5c3f0699ba201eadb471
Korniliy Zervas <korniliyzervas@Korniliys-Laptop.local>
2024-09-10 10:52:58 +0300

    [DSBA24] create reading_data func

--- cl_app.py
+++ cl_app.py
@@ -1,5 +1,29 @@
-# Music recommendation application
+LAST_INDEX = 19
+
+
+def reading_data(path: str = "data/Spotify_Youtube.csv"):
+    big_data = []
+    with open(path) as f:
+        header = [i for i in f.readline().strip().split(',')]
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            if line.split(',')[0].isnumeric():
+                temp_data = [i for i in line.strip().split(',')][:LAST_INDEX]
+                if len(temp_data) >= LAST_INDEX:
+                    big_data.append(temp_data)
+
+    return header, big_data
+
+
+def main():
+    header, data = reading_data()
+    return
+
 
 if __name__ == "__main__":
-    # put your code here
-    print("DSBA-Python-2024")
+    main()
+
+
+
